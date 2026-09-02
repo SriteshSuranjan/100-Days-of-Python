fa4745ec60609c5759322a7c25fbd9904c7beb5d
Sritesh Suranjan <117424084+SriteshSuranjan@users.noreply.github.com>
2026-09-02 08:32:55 +0530

    Day08-Exercise-1-Create-a-Calculator-Solution

---
+++ 08-Day8-Exercise-1-Create-a-Calculator-Solution/main.py
@@ -0,0 +1,9 @@
+a = 7
+b = 3
+print("Addition of", a, "and", b, "is:", a + b)
+print("Subtraction of", a, "and", b, "is:", a - b)
+print("Multiplication of", a, "and", b, "is:", a * b)
+print("Division of", a, "and", b, "is:", a / b)
+print("Floor Division of", a, "and", b, "is:", a // b)
+print("Modulus of", a, "and", b, "is:", a % b)
+print("Exponentiation of", a, "and", b, "is:", a ** b)
