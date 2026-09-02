ab1853db6ede1621871445e5e153d68e40338fb8
Sritesh Suranjan <117424084+SriteshSuranjan@users.noreply.github.com>
2026-09-02 08:32:04 +0530

    Day05-Comments-and-Print

---
+++ 05-Day05-Comments-and-Print/main.py
@@ -0,0 +1,13 @@
+# Hey Sritesh, Please Don't Remove this line.
+# Author: Sritesh Suranjan
+# Course: 100 Days of Python
+'''
+This is a multi-line comment. It can be used to write comments that span multiple lines.
+'''
+"""
+This is also a multi-line comment. It can be used to write comments that span multiple lines.
+"""
+
+print("Hey! I am a \"Good Boy\". \nI am learning Python")
+print("Hey", 6, 7, sep="~", end="003\n")
+print("Hello!")
