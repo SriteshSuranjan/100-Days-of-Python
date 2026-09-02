2fa7c0b59c8fe0ef994af37e3f2f2559690cec8f
Sritesh Suranjan <117424084+SriteshSuranjan@users.noreply.github.com>
2026-09-02 08:32:21 +0530

    Day06-Variables-and-Datatypes

---
+++ 06-Day06-Variables-and-Datatypes/main.py
@@ -0,0 +1,25 @@
+# a = 123
+# a = 1.23
+a = complex(1, 2)
+b = True
+c = "Sritesh"
+d = None 
+print(a)
+a1 = 9
+print(a + a1)
+print("The type of a is", type(a))
+print("The type of b is", type(b))
+print("The type of c is", type(c))
+print("The type of d is", type(d))
+
+list1 = [8, 2.3, [-4, 5], ["apple", "banana"]]
+print(list1)
+print("The type of list1 is", type(list1))
+
+tuple1 = (("parrot", "sparrow"), ("Lion", "Tiger"))
+print(tuple1)
+print("The type of tuple1 is", type(tuple1))
+
+dict1 = {"name":"Sakshi", "age":20, "canVote":True}
+print(dict1)
+print("The type of dict1 is", type(dict1))
