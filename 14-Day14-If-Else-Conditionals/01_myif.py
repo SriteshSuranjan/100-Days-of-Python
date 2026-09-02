840d1118058df42a23065540566e55023158dc4e
Sritesh Suranjan <117424084+SriteshSuranjan@users.noreply.github.com>
2026-09-02 18:13:08 +0530

    Day14-If-Else-Conditional

---
+++ 14-Day14-If-Else-Conditionals/01_myif.py
@@ -0,0 +1,15 @@
+a = int(input("Enter your age: "))
+print("Your age is: ", a)
+# Conditional operators 
+# >, <, >=, <=, ==, !=
+# print(a>18)
+# print(a<=18)
+# print(a==18)
+# print(a!=18)
+if(a>18):
+    print("You can Drive.")
+    print("Yes")
+else:
+    print("You cannot Drive.")
+    print("No")
+print("End of the program.")
