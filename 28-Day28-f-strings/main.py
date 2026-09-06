068c13fc7907986565ae9c69ea52152df42e9b2b
Sritesh Suranjan <117424084+SriteshSuranjan@users.noreply.github.com>
2026-09-06 12:59:32 +0530

    Day28-f-strings

---
+++ 28-Day28-f-strings/main.py
@@ -0,0 +1,11 @@
+txt = "For only {price:.2f} dollars!"
+print(txt.format(price = 49.099))
+
+val = 'Geeks'  
+print(f"{val}for{val} is a portal for {val}.")  
+name = 'Tushar'  
+age = 23  
+print(f"Hello, My name is {name} and I'm {age} years old.")  
+print(f"Hello, My name is {{name}} and I'm {{age}} years old.")  
+
+print(f"{2 * 30}")
