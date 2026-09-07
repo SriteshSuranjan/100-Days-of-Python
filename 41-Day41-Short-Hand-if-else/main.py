eed68e102f3a474ca5c3ae37d8093b3d5690e39e
Sritesh Suranjan <117424084+SriteshSuranjan@users.noreply.github.com>
2026-09-07 16:20:01 +0530

    Day41-Short-Hand-if-else

---
+++ 41-Day41-Short-Hand-if-else/main.py
@@ -0,0 +1,9 @@
+a = 2
+b = 330
+print("A") if a > b else print("B")
+
+a = 330
+b = 330
+print("A") if a > b else print("=") if a == b else print("B")
+
+# print("A") if a > b else "" # If not writing any statement in else atleast give " "
