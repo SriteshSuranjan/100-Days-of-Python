643c3f0f202ef82d98c0b5b2e730cc71a44fb12e
Sritesh Suranjan <117424084+SriteshSuranjan@users.noreply.github.com>
2026-09-08 18:02:49 +0530

    Day 44-How-import-works

---
+++ 44-Day44-How-import-works/main.py
@@ -0,0 +1,16 @@
+# import math
+import math as m
+# from math import sqrt
+# from math import sqrt, pi
+from math import *
+
+import stark as stk
+
+result = m.sqrt(9)
+print(result)  # Output: 3.0
+# print(pi)
+print(m.pi)
+print(dir(m))
+
+stk.welcome()
+print(stk.stark)
---
+++ 44-Day44-How-import-works/stark.py
@@ -0,0 +1,4 @@
+def welcome():
+    print("Hey! You are Welcome My Friend.")
+
+stark = "I am IRON MAN."
